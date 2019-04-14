7f3f308f5c7e76abf2383852c9168effe275902d
duy11nv <duy11nv@gmail.com>
2019-04-14 08:34:23 +0700

    Create random_generator.py

---
+++ random_generator.py
@@ -0,0 +1,13 @@
+# random bit generator circuit in Qis kit
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, execute
+
+qreg = QuantumRegister(1)
+creg = ClassicalRegister(1)
+qcircuit = QuantumCircuit(qreg, creg)
+qcircuit.h(qreg[0])
+qcircuit.measure(qreg[0],creg[0])
+
+result=execute(qcircuit, backend='local_qasm_simulator', shots=1).result()
+        
+print(result.get_counts())
+    
